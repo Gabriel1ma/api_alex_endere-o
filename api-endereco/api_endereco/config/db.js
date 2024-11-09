21ba591e74ee704bc520859d4f60eb5cff67a09a
Gabriel Lima <166053879+Gabriel1ma@users.noreply.github.com>
2024-11-08 21:57:03 -0300

    Add files via upload

---
+++ api-endereco/api_endereco/config/db.js
@@ -0,0 +1,19 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    connectionString: `postgresql://neondb_owner:Xtw5YmhNA7ld@ep-sparkling-credit-a5ey2odc.us-east-2.aws.neon.tech/neondb?sslmode=require`,
+});
+
+module.exports = pool;
+
+const testConnection = async () => {
+    try {
+        const client = await pool.connect();
+        console.log('Conexão ao banco de dados bem-sucedida!');
+        client.release();
+    } catch (err) {
+        console.error('Erro ao conectar ao banco de dados:', err);
+    }
+};
+
+testConnection();
